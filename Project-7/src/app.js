636a37ed693384eb6bd546c42feee0ef2393db70
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-06-28 13:18:53 +0530

    feat: add logging functionality with winston and middleware for request logging

--- Project-7/src/app.js
+++ Project-7/src/app.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("./utility/logger");
+const requestLogger = require("./middleware/requestLogger")
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set("View Engine", ejs);
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/health-check", requestLogger, (req, res) => {
+    res.send("working fine")
+})
 
 module.exports = app;
